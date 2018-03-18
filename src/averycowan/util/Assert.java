5e460fc398ab4300eda3b7ab14da5b57f59e494e
ave4224 <avery@bvp.com>
2018-03-18 12:03:54 -0700

    updated

---
+++ src/averycowan/util/Assert.java
@@ -0,0 +1,62 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 averycowan.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package averycowan.util;
+
+import java.util.function.Supplier;
+
+/**
+ *
+ * @author averycowan
+ */
+public class Assert {
+
+    public static void that(boolean b, String s) {
+        if (!b) {
+            throw new AssertionFailureException(s);
+        }
+    }
+
+    public static void that(boolean b, Object s) {
+        if (!b) {
+            throw new AssertionFailureException(s.toString());
+        }
+    }
+
+    public static void that(boolean b, Supplier<String> s) {
+        if (!b) {
+            throw new AssertionFailureException(s.get());
+        }
+    }
+
+    public static void that(boolean b) {
+        Assert.that(b, "Assertion failed");
+    }
+
+    private static class AssertionFailureException extends RuntimeException {
+
+        public AssertionFailureException(String s) {
+            super(s);
+        }
+    }
+}
---
+++ src/averycowan/util/SuperPair.java
@@ -0,0 +1,56 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 averycowan.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package averycowan.util;
+
+/**
+ *
+ * @author averycowan
+ */
+public class SuperPair<Super, F extends Super, S extends Super> {
+
+    public F _0;
+    public S _1;
+
+    public SuperPair(F first, S second) {
+        _0 = first;
+        _1 = second;
+    }
+
+    public F first() {
+        return _0;
+    }
+
+    public S second() {
+        return _1;
+    }
+
+    public Super value(boolean id) {
+        if (id) {
+            return _0;
+        } else {
+            return _1;
+        }
+    }
+
+}
